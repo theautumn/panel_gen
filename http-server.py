190a0eb8f681deec45c991a445a2ccda152ab2bf
Sarah Autumn <sarah@connectionsmuseum.org>
2018-12-10 23:34:19 -0800

    minor fixes after some http interface testing

--- http-server.py
+++ http-server.py
@@ -12,17 +12,18 @@ def landing():
     if request.method == 'POST':
         command = request.form['command']
         if command == "start":
-            print(">>  Hooray a start button!")
+            print('>>  Got a START request')
             p = subprocess.Popen('python /home/sarah/panel_gen/panel_gen.py -o 5xb -a 8 -v heavy --http', shell=True, preexec_fn=os.setsid)
             return render_template('running.html')
         elif command == "stop":
-            print("<<  Oh no stop everything!")
-            print p
-            os.killpg(os.getpgid(p.pid), signal.SIGTERM)  # Send the signal to all the process groups
-            return render_template('stopped.html')
-
+            print('<<  Got a STOP request.')
+            try:
+                os.killpg(os.getpgid(p.pid), signal.SIGTERM)  # Send the signal to all the process groups
+                return render_template('stopped.html')
+            except (OSError, NameError):
+                print('nothing to terminate. returning to index page')
+                return render_template('index.html')
     return render_template('index.html')
 
-
-app.run()
+app.run(host='0.0.0.0')
 
