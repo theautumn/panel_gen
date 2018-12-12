d8ab1fdcfab458131bd5b2f3cc61e40b003b16f0
Sarah Autumn <sarah@connectionsmuseum.org>
2018-12-12 13:01:34 -0800

    ah dammit i forgot to git add some things

--- http-server.py
+++ http-server.py
@@ -4,6 +4,11 @@
 #
 # Web page created with Skeleton CSS framework
 # http://www.getskeleton.com
+#
+# A note on using Flask: This is insecure. We must only make this
+# available from inside of our network, and we probably shouldn't be
+# letting visitors on to our private wifi anyway.
+#
 #-----------------------------------------------
 
 from flask import Flask, render_template, redirect, request, flash
@@ -27,25 +32,47 @@ class RUN_STRING:
     PANEL = 'python /home/sarah/panel_gen/panel_gen.py -a 4  --http'
     XB5 = 'python /home/sarah/panel_gen/panel_gen.py -o 5xb -t 832 -t 232 -t 275 -a 7 -v heavy --http'
 
-@app.route('/', methods=['GET', 'POST'])
-def landing():
+@app.route('/5xb', methods=['GET', 'POST'])
+def xb5home():
     global p
     if request.method == 'POST':
         command = request.form['command']
         if command == "start":
-            print(bcolors.OKGREEN + '>>  Got a START request' + bcolors.ENDC)
+            print(bcolors.OKGREEN + '>>  Got a START request for 5XB' + bcolors.ENDC)
             p = subprocess.Popen(RUN_STRING.XB5, shell=True, preexec_fn=os.setsid)
-            return render_template('running.html')
+            return render_template('5xb.html', status="Running")
         elif command == "stop":
             try:
-                print(bcolors.FAIL + '<<  Got a STOP request.' + bcolors.ENDC)
-                os.killpg(os.getpgid(p.pid), signal.SIGALRM)  # Send the signal to all the process groups
+                print(bcolors.FAIL + '<<  Got a STOP request for 5XB' + bcolors.ENDC)
+                os.killpg(os.getpgid(p.pid), signal.SIGALRM)
                 p = None
-                return render_template('stopped.html')
+                return render_template('5xb.html', status="Stopped")
             except Exception:
                 print(bcolors.WARNING + 'Nothing to terminate. Returning to index page...' + bcolors.ENDC)
-                return render_template('index.html')
-    return render_template('index.html')
+                return render_template('5xb.html')
+
+    return render_template('5xb.html')
+
+@app.route('/panel', methods=['GET', 'POST'])
+def panelhome():
+    global p
+    if request.method == 'POST':
+        command = request.form['command']
+        if command == "start":
+            print(bcolors.OKGREEN + '>>  Got a START request for PANEL' + bcolors.ENDC)
+            p = subprocess.Popen(RUN_STRING.PANEL, shell=True, preexec_fn=os.setsid)
+            return render_template('panel.html', status="Running")
+        elif command == "stop":
+            try:
+                print(bcolors.FAIL + '<<  Got a STOP request for PANEL' + bcolors.ENDC)
+                os.killpg(os.getpgid(p.pid), signal.SIGALRM)
+                p = None
+                return render_template('panel.html', status="Stopped")
+            except Exception:
+                print(bcolors.WARNING + 'Nothing to terminate. Returning to index page...' + bcolors.ENDC)
+                return render_template('panel.html')
+
+    return render_template('panel.html')
 
 app.run(host='0.0.0.0')
 
