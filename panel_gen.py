6a3ad2a59fb0dd5f9be18a8a6a292c73a04b302d
Sarah Autumn <sarah@connectionsmuseum.org>
2019-03-29 03:10:40 -0700

    add license

--- panel_gen.py
+++ panel_gen.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python
 #---------------------------------------------------------------------#
 #                                                                     #
 #  A call generator thing for the Rainier Panel switch at the         #
